bbbe2821a212b12cff0a03ec0b8d55df954db67c
Oleg Bukovskiy <olbukovskiy@gmail.com>
2022-11-12 16:21:23 +0200

    create folder + basic settings

---
+++ src/App/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { Profile } from './Profile/Profile.js';
+import { Statistics } from './Statistics/Statistics.js';
+import { FriendList } from './FriendList/FriendList.js';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory.js';
+
+function App() {
+  return;
+}
+
+export default App;
